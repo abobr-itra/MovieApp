4b0253276826509ec8c277cf51aa30304d2f1cb3
abobr-itra <a.bobr@itransition.com>
2023-04-11 16:03:23 +0300

    Code refactor

--- MovieApp/MovieApp/Views/AppCoordinatior.swift
+++ MovieApp/MovieApp/Views/AppCoordinatior.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 protocol Coordinator {
+  var navigationController: UINavigationController { get set }
   func start()
   func coordinate(to coordinator: Coordinator)
 }
@@ -14,6 +15,8 @@ extension Coordinator {
 class AppCoordinatior: Coordinator {
   
   private let window: UIWindow
+  var navigationController: UINavigationController = UINavigationController()
+  private var tag = 0
   
   init(window: UIWindow) {
     self.window = window
@@ -27,18 +30,29 @@ class AppCoordinatior: Coordinator {
     
     let searchMovieCoordinator = SearchMovieCoordinator(navigationController: UINavigationController())
     let wishlistCoordinator = WishlistCoordinator(navigationController: UINavigationController())
-    
-    let searchVC = searchMovieCoordinator.navigationController
-    let wishlistVC = wishlistCoordinator.navigationController
-    let searchItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
-    let wishlistItem = UITabBarItem(title: "Wishlist", image: UIImage(systemName: "star"), tag: 1)
-    searchVC.tabBarItem = searchItem
-    wishlistVC.tabBarItem = wishlistItem
+
+    let searchVC = setupViewController(coordinator: searchMovieCoordinator,
+                                       title: "Search",
+                                       image: UIImage(systemName: "magnifyingglass"))
+    let wishlistVC = setupViewController(coordinator: wishlistCoordinator,
+                                         title: "Wishlist",
+                                         image: UIImage(systemName: "star"))
     
     let viewControllers = [searchVC, wishlistVC]
-    
     tabBarController.viewControllers = viewControllers
     
     coordinate(to: searchMovieCoordinator)
+  }
+  
+  private func generateTag() -> Int {
+    tag += 1
+    return tag
+  }
+  
+  private func setupViewController(coordinator: Coordinator, title: String, image: UIImage?) -> UIViewController {
+    let viewController = coordinator.navigationController
+    let tabBarItem = UITabBarItem(title: title, image: image, tag: generateTag())
+    viewController.tabBarItem = tabBarItem
+    return viewController
   }
 }
