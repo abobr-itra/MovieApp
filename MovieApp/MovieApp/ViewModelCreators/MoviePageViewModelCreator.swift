0859084098bb62e89084b8bcb34fcaa8936fd71e
abobr-itra <a.bobr@itransition.com>
2023-07-17 12:57:09 +0300

    Refactor RealmService

--- MovieApp/MovieApp/ViewModelCreators/MoviePageViewModelCreator.swift
+++ MovieApp/MovieApp/ViewModelCreators/MoviePageViewModelCreator.swift
@@ -1,12 +1,15 @@
 import Foundation
+import RealmSwift
 
 class MoviePageViewModelCreator: ViewModelCreatorProtocol {
     
     typealias ViewModel = MoviePageViewModel
     
     func factoryMethod(parser: NetworkPaserProtocol) -> MoviePageViewModel {
+        let realm = try? Realm()
+        let dataService = RealmService(realm: realm)
+
         let networkService = NetworkService(parser: parser)
-        let dataService = RealmService()
         let movieService = MovieService(networkService: networkService)
         return MoviePageViewModel(movieService: movieService, dataService: dataService)
     }
--- MovieApp/MovieApp/Views/Wishlist/ViewModel/WishlistViewModel.swift
+++ MovieApp/MovieApp/Views/Wishlist/ViewModel/WishlistViewModel.swift
@@ -21,18 +21,16 @@ class WishlistViewModel: MovieViewModelProtocol, WishlistViewModelProtocol {
     
     func loadWishlist() {
         onLoading?(true)
-        DispatchQueue.global().async { [weak self] in
-            self?.dataService.getAllObjects(ofType: RealmMovie.self) { result in
-                DispatchQueue.main.async {
-                    self?.onLoading?(false)
-                }
-                switch result {
-                case .success(let data):
-                    self?.moviesDB = data
-                    self?.onDataLoaded?()
-                case .failure(let error):
-                    print("Something went wrong: \(error)")
-                }
+        dataService.getAllObjects(ofType: RealmMovie.self) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.onLoading?(false)
+            }
+            switch result {
+            case .success(let data):
+                self?.moviesDB = data
+                self?.onDataLoaded?()
+            case .failure(let error):
+                print("Something went wrong: \(error)")
             }
         }
     }
