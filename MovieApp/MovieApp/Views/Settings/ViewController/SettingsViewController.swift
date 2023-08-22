d93d809167b9423bd82d9f3528ec5fba43255573
abobr-itra <a.bobr@itransition.com>
2023-08-22 14:49:51 +0300

    Basic implementation of ui

--- MovieApp/MovieApp/Views/Settings/ViewController/SettingsViewController.swift
+++ MovieApp/MovieApp/Views/Settings/ViewController/SettingsViewController.swift
@@ -6,6 +6,24 @@ class SettingsViewController: UIViewController {
     
     private var viewModel: SettingsViewModelProtocol?
     
+    private var profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.borderWidth = 1
+        imageView.layer.masksToBounds = false
+        imageView.layer.cornerRadius = imageView.frame.size.height / 2
+        imageView.clipsToBounds = true
+
+        return imageView
+    }()
+    
+    private let profileTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
     private let tableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(SettingsCell.self,
@@ -23,7 +41,9 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = tableView.backgroundColor
         setupTableView()
+        setupProfile()
     }
     
     // MARK: - Private
@@ -33,7 +53,34 @@ class SettingsViewController: UIViewController {
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.frame = view.bounds
+        tableView.frame = CGRect(x: 0, y: 200, width: view.bounds.width, height: view.bounds.height)
+    }
+    
+    private func setupProfile() {
+        setupProfileImage()
+        setupProfileLabel()
+    }
+    
+    private func setupProfileImage() {
+        profileImage.image = UIImage(named: "profile_placeholder")
+        view.addSubview(profileImage)
+        NSLayoutConstraint.activate([
+            profileImage.widthAnchor.constraint(equalToConstant: 80),
+            profileImage.heightAnchor.constraint(equalToConstant: 80),
+            profileImage.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 15),
+            profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func setupProfileLabel() {
+        profileTitle.text = "Andrey Bobr"
+        view.addSubview(profileTitle)
+        NSLayoutConstraint.activate([
+            profileTitle.widthAnchor.constraint(equalToConstant: 100),
+            profileTitle.heightAnchor.constraint(equalToConstant: 20),
+            profileTitle.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 110),
+            profileTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 }
 
