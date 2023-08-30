f3bb2baeb450409f9eb75e8b155f8a17b24f759b
abobr-itra <a.bobr@itransition.com>
2023-08-30 15:11:19 +0300

    Finish basic custom text field

--- MovieApp/MovieApp/Views/CustomViews/CustomFieldView.swift
+++ MovieApp/MovieApp/Views/CustomViews/CustomFieldView.swift
@@ -2,22 +2,50 @@ import UIKit
 
 class CustomFieldView: UIView {
     
-    var placeholder: String = ""
+    // MARK: - Properties
     
+    private var textFieldTopConstraint: NSLayoutConstraint?
+
     private struct Constants {
         
         static let backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.04)
-        static let mainColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        static let mainColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.87)
+        static let secondaryColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        
+        static let errorColor = UIColor(red: 0.69, green: 0, blue: 0.13, alpha: 1)
     }
     
     private var customTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.tintColor = Constants.mainColor
         return textField
     }()
+    
+    private var fieldLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Constants.secondaryColor
+        label.font = label.font.withSize(10)
+        return label
+    }()
+    
+    private var helperTextLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Constants.secondaryColor
+        label.font = label.font.withSize(10)
+        return label
+    }()
+    
+    private var bottomBorder: UIView?
 
+    // MARK: - Init
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        
+        setupView()
     }
 
     override init(frame: CGRect) {
@@ -25,20 +53,37 @@ class CustomFieldView: UIView {
         
         setupView()
     }
+    
+    // MARK: - Public
+    
+    func setPlaceholder(_ text: String) {
+        customTextField.placeholder = text
+        fieldLabel.text = text
+    }
+    
+    func indecateError() {
+        fieldLabel.textColor = Constants.errorColor
+        bottomBorder?.tintColor = Constants.errorColor
+    }
 
     // MARK: - Private
     
     private func setupView() {
         setupBackground()
         setupTextField()
+        setupLabel()
+        setupHelperText()
     }
     
     private func setupTextField() {
         self.addSubview(customTextField)
+        customTextField.delegate = self
         let mainParagraphStyle = NSMutableParagraphStyle()
         mainParagraphStyle.lineHeightMultiple = 1.19
-        customTextField.attributedPlaceholder = NSAttributedString(string: placeholder,
-                                                                   attributes: [NSAttributedString.Key.foregroundColor : Constants.mainColor])
+        customTextField.attributedPlaceholder = NSAttributedString(string: customTextField.placeholder ?? "",
+                                                                   attributes: [
+                                                                    NSAttributedString.Key.foregroundColor : Constants.secondaryColor
+                                                                   ])
         customTextField.tintColor = Constants.mainColor
         customTextField.attributedText = NSAttributedString(string: "",
                                             attributes: [
@@ -46,9 +91,24 @@ class CustomFieldView: UIView {
                                                 NSAttributedString.Key.paragraphStyle : mainParagraphStyle
                                             ])
         
-        customTextField.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        textFieldTopConstraint = customTextField.topAnchor.constraint(equalTo: self.topAnchor, constant: 10)
+        textFieldTopConstraint?.isActive = true
         customTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
         customTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
+    }
+    
+    private func setupLabel() {
+        self.addSubview(fieldLabel)
+        fieldLabel.isHidden = true
+        fieldLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
+        fieldLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+    }
+    
+    private func setupHelperText() {
+        self.addSubview(helperTextLabel)
+        //helperTextLabel.isHidden = true
+        helperTextLabel.topAnchor.constraint(equalTo: self.bottomAnchor, constant: 5).isActive = true
+        helperTextLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
     }
     
     private func setupBackground() {
@@ -56,11 +116,19 @@ class CustomFieldView: UIView {
         clipsToBounds = true
         layer.cornerRadius = 15
         layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        
-        let bottomLine = CALayer()
-        bottomLine.frame = CGRect(x: 0, y: bounds.size.height - 1, width: bounds.size.width, height: 1)
-        bottomLine.backgroundColor = Constants.mainColor.cgColor
-       // borderStyle = .none
-        layer.addSublayer(bottomLine)
+        bottomBorder = addBottomBorder(color: Constants.mainColor, borderLineSize: 1)
+    }
+}
+
+extension CustomFieldView: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        fieldLabel.isHidden = false
+        textFieldTopConstraint?.constant = 20
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        fieldLabel.isHidden = true
+        textFieldTopConstraint?.constant = 10
     }
 }
--- MovieApp/MovieApp/Views/UITestView/TestViewController.swift
+++ MovieApp/MovieApp/Views/UITestView/TestViewController.swift
@@ -1,21 +1,7 @@
 import UIKit
 
 class TestViewController: UIViewController {
-    
-    private let customTextField: CustomTextField = {
-        let textField = CustomTextField(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.placeholder = "Label"
-        return textField
-    }()
-    
-    private let customTextField2: CustomTextField = {
-        let textField = CustomTextField(frame: .zero)
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.placeholder = "Label"
-        return textField
-    }()
-    
+
     private let customField: CustomFieldView = {
         let field = CustomFieldView(frame: .zero)
         field.translatesAutoresizingMaskIntoConstraints = false
@@ -27,18 +13,12 @@ class TestViewController: UIViewController {
 
         view.backgroundColor = .systemBackground
         
-      //  view.addSubview(customTextField)
         setupView()
-//        customTextField2.heightAnchor.constraint(equalToConstant: 48).isActive = true
-//        customTextField2.widthAnchor.constraint(equalToConstant: 200).isActive = true
-//        customTextField2.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        customTextField2.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
     }
     
     private func setupView() {
         view.addSubview(customField)
-        customField.placeholder = "Label"
+        customField.setPlaceholder("Label")
         customField.heightAnchor.constraint(equalToConstant: 48).isActive = true
         customField.widthAnchor.constraint(equalToConstant: 200).isActive = true
         customField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
