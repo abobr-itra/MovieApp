130ec11902006040981fc092bfbc0eca12b7cd5c
abobr-itra <a.bobr@itransition.com>
2023-09-06 11:36:32 +0300

    Move constants to separate file

--- MovieApp/MovieApp/Views/CustomViews/CustomFieldView.swift
+++ MovieApp/MovieApp/Views/CustomViews/CustomFieldView.swift
@@ -5,21 +5,6 @@ class CustomFieldView: UIView {
     // MARK: - Properties
     
     private var textFieldTopConstraint: NSLayoutConstraint?
-
-    private struct Constants {
-        
-        static let fielCornerRadius: CGFloat = 15
-        static let leadingPadding: CGFloat = 16
-        
-        static let labelDefaultFontSize: CGFloat = 15
-        static let labelSmallFontSize: CGFloat = 10
-        
-        static let backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.04)
-        static let mainColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.87)
-        static let secondaryColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
-        
-        static let errorColor = UIColor(red: 0.69, green: 0, blue: 0.13, alpha: 1)
-    }
     
     private var textFieldView: UIView = {
         let view = UIView()
@@ -30,8 +15,8 @@ class CustomFieldView: UIView {
     private var customTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.textColor = Constants.mainColor
-        textField.tintColor = Constants.mainColor
+        textField.textColor = Constants.CustomField .mainColor
+        textField.tintColor = Constants.CustomField.mainColor
         return textField
     }()
     
@@ -41,15 +26,15 @@ class CustomFieldView: UIView {
     private var fieldLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = Constants.secondaryColor
-        label.font = label.font.withSize(Constants.labelDefaultFontSize)
+        label.textColor = Constants.CustomField.secondaryColor
+        label.font = label.font.withSize(Constants.CustomField.labelDefaultFontSize)
         return label
     }()
     
     private var helperTextLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = Constants.secondaryColor
+        label.textColor = Constants.CustomField.secondaryColor
         label.font = label.font.withSize(10)
         return label
     }()
@@ -81,17 +66,17 @@ class CustomFieldView: UIView {
     }
     
     func indecateError() {
-        fieldLabel.textColor = Constants.errorColor
-        bottomBorder?.backgroundColor = Constants.errorColor
-        helperTextLabel.textColor = Constants.errorColor
-        customTextField.tintColor = Constants.errorColor
+        fieldLabel.textColor = Constants.CustomField.errorColor
+        bottomBorder?.backgroundColor = Constants.CustomField.errorColor
+        helperTextLabel.textColor = Constants.CustomField.errorColor
+        customTextField.tintColor = Constants.CustomField.errorColor
     }
     
     func hideError() {
-        fieldLabel.textColor = Constants.mainColor
-        bottomBorder?.backgroundColor = Constants.mainColor
-        helperTextLabel.textColor = Constants.secondaryColor
-        customTextField.tintColor = Constants.mainColor
+        fieldLabel.textColor = Constants.CustomField.mainColor
+        bottomBorder?.backgroundColor = Constants.CustomField.mainColor
+        helperTextLabel.textColor = Constants.CustomField.secondaryColor
+        customTextField.tintColor = Constants.CustomField.mainColor
     }
 
     // MARK: - Private
@@ -123,8 +108,8 @@ class CustomFieldView: UIView {
         
         textFieldTopConstraint = customTextField.topAnchor.constraint(equalTo: self.topAnchor, constant: 10)
         textFieldTopConstraint?.isActive = true
-        customTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.leadingPadding).isActive = true
-        customTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Constants.leadingPadding).isActive = true
+        customTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.CustomField.leadingPadding).isActive = true
+        customTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Constants.CustomField.leadingPadding).isActive = true
     }
     
     private func setupLabel() {
@@ -136,15 +121,15 @@ class CustomFieldView: UIView {
     
     private func makeLabelDefault() {
         UIView.animate(withDuration: 5) {
-            self.fieldLabel.font = self.fieldLabel.font.withSize(Constants.labelDefaultFontSize)
+            self.fieldLabel.font = self.fieldLabel.font.withSize(Constants.CustomField.labelDefaultFontSize)
             self.labelTopConstraint = self.fieldLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 15)
-            self.labelLeadingConstraint = self.fieldLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.leadingPadding)
+            self.labelLeadingConstraint = self.fieldLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.CustomField.leadingPadding)
         }
     }
     
     private func makeLabelSmall() {
         UIView.animate(withDuration: 5) {
-            self.fieldLabel.font = self.fieldLabel.font.withSize(Constants.labelSmallFontSize)
+            self.fieldLabel.font = self.fieldLabel.font.withSize(Constants.CustomField.labelSmallFontSize)
             self.labelTopConstraint?.constant = 8
         }
     }
@@ -153,15 +138,15 @@ class CustomFieldView: UIView {
         self.addSubview(helperTextLabel)
         helperTextLabel.isHidden = true
         helperTextLabel.topAnchor.constraint(equalTo: self.bottomAnchor, constant: -15).isActive = true
-        helperTextLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.leadingPadding).isActive = true
+        helperTextLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.CustomField.leadingPadding).isActive = true
     }
     
     private func setupBackground() {
-        textFieldView.backgroundColor = Constants.backgroundColor
+        textFieldView.backgroundColor = Constants.CustomField.backgroundColor
         textFieldView.clipsToBounds = true
-        textFieldView.layer.cornerRadius = Constants.fielCornerRadius
+        textFieldView.layer.cornerRadius = Constants.CustomField.fielCornerRadius
         textFieldView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        bottomBorder = textFieldView.addBottomBorder(color: Constants.mainColor, borderLineSize: 1)
+        bottomBorder = textFieldView.addBottomBorder(color: Constants.CustomField.mainColor, borderLineSize: 1)
     }
 }
 
